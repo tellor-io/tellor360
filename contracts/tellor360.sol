5282a9eb41e07202bc95cdd3f70c7b8a204ba525
Nicholas Fett <nicholasfett@Nicholass-MBP.hsd1.md.comcast.net>
2022-05-06 10:17:19 -0400

    init

---
+++ contracts/tellor360.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.3;
+
+//need transition and token functions and autoMinting functions
+
+contract Tellor360 is TellorStorage, TellorVars{
+
+
+
+}
