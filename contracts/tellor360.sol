7c9969f8ed16891d513e5daa9ada20467b02f657
brendaloya <l.loya.b@gmail.com>
2022-06-07 23:20:00 -0400

    added the _isValid oracle contract before proposing it

--- contracts/tellor360.sol
+++ contracts/tellor360.sol
@@ -32,7 +32,7 @@ contract Tellor360 is BaseToken, NewTransition{
         //then when we switch, the governance switch can be instantaneous
         //no need for the dispute transistion...but be sure disputes work on both contracts during those 12 hours
        //BL--ok so for the update to happen 
-       //old tellor flex id 1 value has to be 12 hours old before we can init 360
+       //old tellorx id 1 value has to be 12 hours old before we can init 360
         uint256 _id = 1;
         uint256 _firstTimestamp = IOracle(_flexAddress).getTimestampbyQueryIdandIndex(bytes32(_id),0);
         require(block.timestamp - _firstTimestamp >= 12 hours, "contract should be at least 12 hours old");
@@ -41,39 +41,13 @@ contract Tellor360 is BaseToken, NewTransition{
         uints[keccak256("_LAST_RELEASE_TIME_TEAM")] = block.timestamp;
         uints[keccak256("_LAST_RELEASE_TIME_DAO")] = block.timestamp;
         //mint a few people some tokens (those locked)- These addresses accidentally sent TRB to the
-        //oracle contract and are being reimbursed with this mint ??? BL
+        //oracle contract and are being reimbursed with this mint--BL
         //triple check: https://docs.google.com/spreadsheets/d/1z1GO_9cWRBbWxq651Z7FLoA6iI1nWE4lEHB9OPrZjko/edit#gid=0
         _doMint(address(0x3aa39f73D48739CDBeCD9EB788D4657E0d6a6815), 2.26981073 ether);
         _doMint(address(0xdbbAEee590a2744AfC0112ea3bdD89474f476eDa), 5.16836759 ether);
         _doMint(address(0x503828976D22510aad0201ac7EC88293211D23Da), 11.204 ether);
         _doMint(address(0xEf7353B92BE7CC840B5b2A190B3a555277Fc18c9), 68.55985987 ether);
         _doMint(address(0x7a11CDA496cC596E2241319982485217Cad3996C), 695.0062834 ether);
-    }
-
-    /**
-     * @dev Use this function to update the oracle contract
-     */
-    function updateOracleAddress() external {
-        bytes32 _queryID = keccak256(abi.encode("TellorOracleAddress"));
-        bytes memory _currentOracleAddress;
-        _currentOracleAddress = IOracle(addresses[_ORACLE_CONTRACT]).retrieveData(_queryID, block.timestamp - 12 hours);
-        address _currentOracle = abi.decode(_currentOracleAddress,(address));
-        //If the oracle address being reported is the same as the proposed oracle then update the oracle contract 
-        //only if 7 days have passed since the new oracle address was made official
-        //and if 12 hours have passed since query id 1 was last reported on the soon to be deprecated oracle contract
-        if(_currentOracle == addresses[keccak256("_PROPOSED_ORACLE")]){
-            require(block.timestamp > uints[keccak256("_TIME_PROPOSED_UPDATED")] + 7 days);
-            uint256 _firstTimestamp = IOracle(_currentOracle).getTimestampbyQueryIdandIndex(bytes32(uint256(1)),0);
-            require(block.timestamp - _firstTimestamp >= 12 hours, "contract should be at least 12 hours old");
-            addresses[_ORACLE_CONTRACT] = _currentOracle;
-            emit NewOracleAddress(_currentOracle, block.timestamp);
-        }
-        //otherwise if the current oracle is not the proposed oracle, then propose it and 
-        //start the clock on the 7 days before it can be made official 
-        else{
-            addresses[keccak256("_PROPOSED_ORACLE")] = _currentOracle;
-            uints[keccak256("_TIME_PROPOSED_UPDATED")] = block.timestamp;
-        }
     }
  
     /**
@@ -98,6 +72,49 @@ contract Tellor360 is BaseToken, NewTransition{
         _doMint(addresses[_ORACLE_CONTRACT], _releasedAmount);
     }
 
+    //TODO: be sure to test we can't just print tokens
+    /**
+     * @dev This function allows team to gain control of any tokens sent directly to this 
+     * contract (and send them back))
+     */
+    function transferOutOfContract() external{
+        _doTransfer(address(this),addresses[_OWNER],balanceOf(address(this)));
+    }
+
+    /**
+     * @dev Use this function to update the oracle contract
+     */
+    function updateOracleAddress() external {
+        bytes32 _queryID = keccak256(abi.encode("TellorOracleAddress"));
+        bytes memory _currentOracleAddress;
+        _currentOracleAddress = IOracle(addresses[_ORACLE_CONTRACT]).retrieveData(_queryID, block.timestamp - 12 hours);
+        address _currentOracle = abi.decode(_currentOracleAddress,(address));
+        //If the oracle address being reported is the same as the proposed oracle then update the oracle contract 
+        //only if 7 days have passed since the new oracle address was made official
+        //and if 12 hours have passed since query id 1 was last reported on the soon to be deprecated oracle contract
+        if(_currentOracle == addresses[keccak256("_PROPOSED_ORACLE")]){
+            require(block.timestamp > uints[keccak256("_TIME_PROPOSED_UPDATED")] + 7 days);
+            uint256 _firstTimestamp = IOracle(_currentOracle).getTimestampbyQueryIdandIndex(bytes32(uint256(1)),0);
+            require(block.timestamp - _firstTimestamp >= 12 hours, "contract should be at least 12 hours old");
+            addresses[_ORACLE_CONTRACT] = _currentOracle;
+            emit NewOracleAddress(_currentOracle, block.timestamp);
+        }
+        //Otherwise if the current oracle is not the proposed oracle, then propose it and 
+        //start the clock on the 7 days before it can be made official 
+        else{
+            require(_isValid(_currentOracle));
+            addresses[keccak256("_PROPOSED_ORACLE")] = _currentOracle;
+            uints[keccak256("_TIME_PROPOSED_UPDATED")] = block.timestamp;
+        }
+    }
+
+    /**
+     * @dev Used during the upgrade process to verify valid Tellor Contracts
+     */
+    function verify() external pure returns (uint256) {
+        return 9999;
+    }
+
     /**
      * @dev Used during the upgrade process to verify valid Tellor Contracts and ensure
      * they have the right signature
@@ -113,22 +130,6 @@ contract Tellor360 is BaseToken, NewTransition{
             "New contract is invalid"
         );
         return true;
-    }
-
-    /**
-     * @dev Used during the upgrade process to verify valid Tellor Contracts
-     */
-    function verify() external pure returns (uint256) {
-        return 9999;
-    }
-
-    //TODO: be sure to test we can't just print tokens
-    /**
-     * @dev This function allows team to gain control of any tokens sent directly to this 
-     * contract (and send them back))
-     */
-    function transferOutOfContract() external{
-        _doTransfer(address(this),addresses[_OWNER],balanceOf(address(this)));
     }
 
 }
